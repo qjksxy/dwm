e05445db16e303fd7b6004fadb5f0cdb809f5695
piner <qjksxy@163.com>
2023-03-25 21:31:35 +0800

    feat: :sparkles: 修改了间距

--- config.h
+++ config.h
@@ -11,13 +11,13 @@ static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 8;        /* horiz inner gap between windows */
 static const unsigned int gappiv    = 8;        /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappoh    = 8;        /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 8;        /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int vertpad            = 10;       /* vertical padding of bar */
-static const int sidepad            = 10;       /* horizontal padding of bar */
+static const int vertpad            = 8;        /* vertical padding of bar */
+static const int sidepad            = 8;        /* horizontal padding of bar */
 static const char *fonts[]          = { "Sauce Code Pro Nerd Font:size=16" };
 static const char dmenufont[]       = "Sauce Code Pro Nerd Font:size=14";
 static const char col_gray1[]       = "#222222";
